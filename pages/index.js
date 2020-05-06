fc6fce45d2683fddeea24640fbfc00a71cfacd64
Frederick Johnson <fritzj96@gmail.com>
2020-05-05 19:41:21 -0700

    fiddling with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (<div className="p-4 shadow rounded bg-white">
+  return (
+  <main className="container mx-auto"><div className="p-4 shadow rounded bg-white">
     <h1 className="text-purple-500 leading-normal">Next.js</h1>
     <p className="text-gray-500">with TailwindCSS</p>
   </div>
+  </main>
   )
 }
