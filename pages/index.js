cb04423decd6537088d8930a3a8d6363d62402a8
Frederick Johnson <fritzj96@gmail.com>
2020-05-05 19:32:07 -0700

    added: minimal starting contnet

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (<div />
+  return (<div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple-500 leading-normal">Next.js</h1>
+    <p className="text-gray-500">with TailwindCSS</p>
+  </div>
   )
 }
