8a2a26cf71973606a9508f4875d57e1ee2e00072
Frederick Johnson <fritzj96@gmail.com>
2020-05-05 19:35:50 -0700

    removed: unused import for now

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (<div className="p-4 shadow rounded bg-white">
     <h1 className="text-purple-500 leading-normal">Next.js</h1>
