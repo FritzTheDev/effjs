af793771b258b2ab7b340614216b66e597de3cb4
Frederick Johnson <fritzj96@gmail.com>
2020-05-12 10:54:28 -0700

    extracted the IndexPost component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,6 @@
 import { Header } from "../../lib/Header";
 import Link from "next/link";
+import { IndexPost } from "../../lib/IndexPost";
 
 export default () => (
   <>
@@ -7,18 +8,12 @@ export default () => (
     <main className="container mx-auto">
       <h1 className="text-center mt-4 text-4xl font-display">Blog Archives</h1>
       <section id="posts">
-        <Link href="/blog/next-blog-1">
-          <article className="bg-gray-400 hover:bg-orange-400 mx-5 md:w-1/2 lg:w-1/4 p-5">
-            <img
-              className="rounded-lg w-auto h-auto mx-auto"
-              src="/next-blog-1-screenshot.png"
-            />
-            <header className="text-xl text-center">
-              How To Build A Next.js Blog #1
-            </header>
-            <p className="text-center">Setting Up Next & TailwindCSS</p>
-          </article>
-        </Link>
+        <IndexPost
+          postHref="/blog/next-with-tailwind"
+          imgSrc="/next-with-tailwind-1.png"
+          title="Using Next.js with TailwindCSS"
+          subtitle="Setting Up A Project From Scratch"
+        />
       </section>
     </main>
   </>
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import { Header } from "../lib/Header";
 export default function Home() {
   return (
     <>
-    <Header />
-    <main className="container mx-auto">
-      <div className="bg-black">test</div>
-    </main>
+      <Header />
+      <main className="container mx-auto">
+        <div className="bg-black">test</div>
+      </main>
     </>
   );
 }
