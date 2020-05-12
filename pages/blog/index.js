03dc069ebe348b037d50a6b9e15e6081f5df172d
Frederick Johnson <fritzj96@gmail.com>
2020-05-12 11:46:48 -0700

    changes to tailwind stuff

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,24 +1,24 @@
 import { Header } from "../../lib/Header";
-import Link from "next/link";
 import { IndexPost } from "../../lib/IndexPost";
 
 export default () => (
   <>
     <Header />
     <main className="container mx-auto">
-      <h1 className="text-center mt-4 text-4xl font-display">Blog Archives</h1>
+      <h1 className="text-center mt-4 text-4xl font-display">The EffJS Blog</h1>
       <section id="posts" className="flex flex-wrap">
         <IndexPost
           postHref="/blog/next-with-tailwind"
           imgSrc="/next-with-tailwind-1.png"
           title="Using Next.js with TailwindCSS"
           subtitle="Setting Up A Project From Scratch"
+          date="May 12, 2020"
         />
         <IndexPost
           postHref="/blog/next-with-tailwind"
           imgSrc="/next-with-tailwind-1.png"
-          title="Using Next.js with TailwindCSS"
-          subtitle="Setting Up A Project From Scratch"
+          title="Setting Up MDX and Next.js"
+          subtitle="Learn To Use Markdown and JSX Together To Build A Blog"
         />
         <IndexPost
           postHref="/blog/next-with-tailwind"
