a8987f0448fb88ad776fd8312f5c308586cfaa0c
Frederick Johnson <fritzj96@gmail.com>
2020-05-12 10:29:57 -0700

    added: a screenshot to public & progress on the mock blog index

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,17 +1,24 @@
 import { Header } from "../../lib/Header";
+import Link from "next/link";
 
 export default () => (
   <>
     <Header />
     <main className="container mx-auto">
       <h1 className="text-center mt-4 text-4xl font-display">Blog Archives</h1>
-      <section className="flex flex-wrap">
-        <article className="bg-gray-300 mx-5">
-          <header className="text-xl">Build a Next.js Blog #1 - Next + MDX</header>
-        </article>
-        <article className="bg-gray-300 mx-5">
-          <header className="text-xl">Build a Next.js Blog #1 - Next + MDX</header>
-        </article>
+      <section id="posts">
+        <Link href="/blog/next-blog-1">
+          <article className="bg-gray-400 hover:bg-orange-400 mx-5 md:w-1/2 lg:w-1/4 p-5">
+            <img
+              className="rounded-lg w-auto h-auto mx-auto"
+              src="/next-blog-1-screenshot.png"
+            />
+            <header className="text-xl text-center">
+              How To Build A Next.js Blog #1
+            </header>
+            <p className="text-center">Setting Up Next & TailwindCSS</p>
+          </article>
+        </Link>
       </section>
     </main>
   </>
