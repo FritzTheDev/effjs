f723ce48972e1a3c0736e7c65caf8403af7d6687
Frederick Johnson <fritzj96@gmail.com>
2020-05-11 18:36:28 -0700

    added: css changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-  <main className="container mx-auto"><div className="p-4 shadow rounded bg-white">
-    <h1 className="text-purple-500 leading-normal">Next.js</h1>
-    <p className="text-gray-500">with TailwindCSS</p>
-  </div>
-  </main>
-  )
+    <header className="container mx-auto">
+      <div className="px-6 py-2 shadow bg-blue-600">
+        <h1 className="text-white text-3xl font-display leading-normal">Effective JS</h1>
+        <h4 className="text-white text-xl font-display leading-normal">Javascript To Get The Job Done.</h4>
+      </div>
+    </header>
+  );
 }
