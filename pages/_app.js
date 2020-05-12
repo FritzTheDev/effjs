a46d50c6e283f8cbad93342ca7c2610d4ae72bfd
Frederick Johnson <fritzj96@gmail.com>
2020-05-11 18:42:58 -0700

    added: Prism suppport

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/index.css'
+import { MDXProvider } from "@mdx-js/react";
+
+const components = {
+  pre: props => <div {...props} />,
+  code: props => <pre style={{color: 'tomato'}} {...props} />
+}
+
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <MdxProvider components={components}><Component {...pageProps} /></MdxProvider>
 }
