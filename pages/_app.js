8ac7889174e2e8a04807f8f6b24cb5a9b7c4ddd1
Frederick Johnson <fritzj96@gmail.com>
2020-05-15 13:59:50 -0700

    got code blocks working!

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
-import '../styles/index.css'
+import "../styles/index.css";
 import { MDXProvider } from "@mdx-js/react";
+import { Header } from "../lib/Header";
+import { CodeBlock } from "../lib/CodeBlock";
 
 const components = {
-  pre: props => <div {...props} />,
-  code: props => <pre style={{color: 'tomato'}} {...props} />
-}
-
+  wrapper: (props) => (
+    <>
+      <Header />
+      <div className="container mx-auto">
+        <main {...props} />
+      </div>
+    </>
+  ),
+  h1: (props) => <h1 className="text-4xl text-center" {...props} />,
+  code: (props) => <CodeBlock {...props} />,
+  pre: (props) => <div {...props} />,
+};
 
 export default function MyApp({ Component, pageProps }) {
-  return <MDXProvider components={components}><Component {...pageProps} /></MDXProvider>
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  );
 }
