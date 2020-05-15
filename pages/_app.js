435f287af9256f4abe50d8a3d9e124f3ae0a26fc
Frederick Johnson <fritzj96@gmail.com>
2020-05-15 14:21:47 -0700

    progress on the article + feeling out what I want the md to be styled like

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,15 @@ const components = {
   wrapper: (props) => (
     <>
       <Header />
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4">
         <main {...props} />
       </div>
     </>
   ),
-  p: (props) => <p className="py-1 mx-3" {...props} />,
-  h1: (props) => <h1 className="text-4xl mx-3 text-center" {...props} />,
-  h2: (props) => <h2 className="text-3xl mx-3 text-center" {...props} />,
-  h3: (props) => <h2 className="text-2xl mx-3 text-center" {...props} />,
+  p: (props) => <p className="my-1" {...props} />,
+  h1: (props) => <h1 className="text-4xl mx-3 my-4 text-center" {...props} />,
+  h2: (props) => <h2 className="text-3xl mx-3 my-4 text-center" {...props} />,
+  h3: (props) => <h2 className="text-2xl mx-3 my-4 text-center" {...props} />,
   h4: (props) => <h2 className="text-xl mx-3 text-center" {...props} />,
   h5: (props) => <h2 className="mx-3 text-center" {...props} />,
   code: (props) => <CodeBlock {...props} />,
