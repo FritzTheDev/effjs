d22f892e787db5d0a31f6b96b689fffadb2b576a
Frederick Johnson <fritzj96@gmail.com>
2020-05-15 14:14:51 -0700

    added: changes to the md thingies

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,12 @@ const components = {
       </div>
     </>
   ),
-  p: (props) => <p className="py-1 text-lg mx-3" {...props} />,
+  p: (props) => <p className="py-1 mx-3" {...props} />,
   h1: (props) => <h1 className="text-4xl mx-3 text-center" {...props} />,
   h2: (props) => <h2 className="text-3xl mx-3 text-center" {...props} />,
-  h5: (props) => <h2 className="text-xl mx-3 text-center" {...props} />,
+  h3: (props) => <h2 className="text-2xl mx-3 text-center" {...props} />,
+  h4: (props) => <h2 className="text-xl mx-3 text-center" {...props} />,
+  h5: (props) => <h2 className="mx-3 text-center" {...props} />,
   code: (props) => <CodeBlock {...props} />,
   pre: (props) => <div {...props} />,
 };
