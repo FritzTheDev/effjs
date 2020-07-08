f6c7b984b2e1dad3863410cd82b101357a6bced8
Frederick Johnson <fritzj96@gmail.com>
2020-07-07 21:07:12 -0700

    added: withMdxEnhanced

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
-const withMDXFm = require("next-mdx-frontmatter");
-module.exports = withMDXFm()({ pageExtensions: ["js", "md", "mdx"] });
+const withMdxEnhanced = require('next-mdx-enhanced')
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  fileExtensions: ['mdx'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
+  },
+})(/* your normal nextjs config */)
