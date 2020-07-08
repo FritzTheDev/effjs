42c4df29a7f105689c86e45c9f65d0ea58ec0e98
Frederick Johnson <fritzj96@gmail.com>
2020-07-07 21:05:40 -0700

    lol this is awful

--- next.config.js
+++ next.config.js
@@ -1,4 +1,2 @@
-const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
-module.exports = withMDX({
-  pageExtensions: ['js', 'md', 'mdx']
-});
+const withMDXFm = require("next-mdx-frontmatter");
+module.exports = withMDXFm()({ pageExtensions: ["js", "md", "mdx"] });
